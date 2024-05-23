62b058e74bd7cafe399157cbfd0497ba94ec2526
Flore Vancompernolle Vromman <flore.vancompernolle@uclouvain.be>
2024-05-23 14:56:44 -0400

    Score to data
    Ajouter la colonne avec le score de santé mentale normalisé aux données et retirer tous les répondants qui n'ont pas de score (car ils n'ont pas complété les questions permettant de l'obtenir)

--- code/eda.R
+++ code/eda.R
@@ -1,11 +1,20 @@
-# On ne prend que les répondants qui ont fini le questionnaire
-data <- data_raw %>%
-  filter(Finished == 1)
 
-# Aperçu des valeurs pour chaque variable
-summary(data)
+data <- data_raw
+
+# Ajouter la colonne de score
+data$score_norm <- data_score$score_norm
+
+# On ne prend que les répondants qui ont fini le questionnaire
+data <- data %>%
+  filter(Finished == 1)
 
 # Vérifier les valeurs manquantes
 missing_values <- colSums(is.na(data))
 missing_values[missing_values > 0]
 
+# Retirer les répondants qui n'ont pas de score (car NA dans le questionnaire)
+data <- data %>%
+  filter(!is.na(data$score_norm))
+
+# Aperçu des valeurs pour chaque variable
+summary(data)
