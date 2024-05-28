d33405109c296479374f1b8621f1a464aeb6cd6d
Flore Vancompernolle Vromman <flore.vancompernolle@uclouvain.be>
2024-05-28 13:59:30 -0400

    Update path file raw

--- code/check_na.R
+++ code/check_na.R
@@ -1,8 +1,8 @@
 library(dplyr)
 
 # Read the SPSS data file and filter for respondents with Finished variable set to TRUE
-data <- haven::read_sav("_SharedFolder_datagotchi-santé/data/raw/Datagotchi-Santé_Pilote_May 21, 2024_11.55.sav") %>%
-  filter(Finished == 1)
+data <- haven::read_sav("_SharedFolder_datagotchi-santé/data/raw/datagotchi_sante/data_raw.sav")  %>% 
+  filter(code == "complete")
 
 data_missing_values <- sondr::qualtrics_na_counter(data)
 
--- code/codebook.R
+++ code/codebook.R
@@ -1,4 +1,5 @@
-data <- haven::read_sav("_SharedFolder_datagotchi-santé/data/raw/Datagotchi-Santé_Pilote_May 21, 2024_11.55.sav")
+data <- haven::read_sav("_SharedFolder_datagotchi-santé/data/raw/datagotchi_sante/data_raw.sav")  %>% 
+  filter(code == "complete")
 codebook <- sondr::sav_to_codebook(data)
 
 write.csv(codebook, "_SharedFolder_datagotchi-santé/data/raw/codebook.csv")
