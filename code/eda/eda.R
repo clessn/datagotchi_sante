bfc9494c4c6b4e319f1e9bdd8dc1683b26c5eb6f
Flore Vancompernolle Vromman <flore.vancompernolle@uclouvain.be>
2024-05-28 14:57:51 -0400

    Experimental data analysis file

--- code/eda/eda.R
+++ code/eda/eda.R
@@ -1,16 +1,19 @@
+# Experimental data analysis
 
-data <- data_clean
+# Load data
+data_clean <- readRDS("_SharedFolder_datagotchi-santé/data/clean/datagotchi-sante_clean.rds")
 
-# Ajouter la colonne de score
-data$score_norm <- data_score$score_norm
-
-# Vérifier les valeurs manquantes
-missing_values <- colSums(is.na(data))
+# Check missing values
+missing_values <- colSums(is.na(data_clean))
 missing_values[missing_values > 0]
 
-# Retirer les répondants qui n'ont pas de score (car NA dans le questionnaire)
-data <- data %>%
-  filter(!is.na(data$score_norm))
+# Remove observations without score (because of NA in the questionnaire)
+data <- data_clean %>%
+  filter(!is.na(data_clean$score_norm))
+
+# Check missing values
+missing_values <- colSums(is.na(data))
+missing_values[missing_values > 0]
 
 # Aperçu des valeurs pour chaque variable
 summary(data)
