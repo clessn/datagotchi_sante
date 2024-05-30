2ad49485cb1d512da29323ad245e2f566a1b5923
Corentin Vande Kerckhove <corentinvdk@gmail.com>
2024-05-30 13:12:36 -0400

    prepare paths for machine learning work

--- code/ml/constants.py
+++ code/ml/constants.py
@@ -1,2 +1,12 @@
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+# Load the .env file
+load_dotenv()
+
 class Constants:
+    DATA_PATH = Path(os.getenv('PATH'))
+    RAW_PATH = DATA_PATH / 'raw'
+    ML_PATH = DATA_PATH / 'ml'
 
