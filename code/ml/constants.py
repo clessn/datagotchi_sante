ac856c053a3abaf86ab370f38e35c6783e836c64
Corentin Vande Kerckhove <corentinvdk@gmail.com>
2024-05-30 13:39:43 -0400

    automatically clean python code with make command

--- code/ml/constants.py
+++ code/ml/constants.py
@@ -6,11 +6,12 @@ from dotenv import load_dotenv
 # Load the .env file
 load_dotenv()
 
+
 class Constants:
     # Paths
-    DATA_PATH = Path(os.getenv('DATA_PATH'))
-    RAW_PATH = DATA_PATH / 'raw'
-    ML_PATH = DATA_PATH / 'ml'
+    DATA_PATH = Path(os.getenv("DATA_PATH"))
+    RAW_PATH = DATA_PATH / "raw"
+    ML_PATH = DATA_PATH / "ml"
 
     # Filenames
-    RAW_FILENAME = 'data_raw.sav'
+    RAW_FILENAME = "data_raw.sav"
--- code/ml/loaders.py
+++ code/ml/loaders.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 from constants import Constants as C
 
@@ -6,6 +5,7 @@ from constants import Constants as C
 def load_raw_data():
     df = pd.read_spss(C.RAW_PATH / C.RAW_FILENAME)
     breakpoint()
+
 
 if __name__ == "__main__":
     load_raw_data()
