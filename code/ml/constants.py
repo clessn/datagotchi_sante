3111b2e6b8263e1c55103a6ae45241c2fd17ba76
Corentin Vande Kerckhove <corentinvdk@gmail.com>
2024-05-30 13:38:52 -0400

    sort packages

--- code/ml/constants.py
+++ code/ml/constants.py
@@ -1,6 +1,7 @@
-from dotenv import load_dotenv
 import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Load the .env file
 load_dotenv()
--- code/ml/loaders.py
+++ code/ml/loaders.py
@@ -1,6 +1,7 @@
-import pandas as pd
 
+import pandas as pd
 from constants import Constants as C
+
 
 def load_raw_data():
     df = pd.read_spss(C.RAW_PATH / C.RAW_FILENAME)
