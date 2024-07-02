430b48fb9ec766bae3270562b98cd7c63c162634
Flore Vancompernolle Vromman <flore.vancompernolle@uclouvain.be>
2024-07-02 15:15:04 -0400

    Remove one category for nominal variables

--- code/ml/configs/score_feature.py
+++ code/ml/configs/score_feature.py
@@ -1,18 +1,10 @@
 class ScoreFeatureConfig:
     # Versioning
     RUN_TYPE = "REAL_FOLDER_NAME"  # SANDBOX_FOLDER_NAME or REAL_FOLDER_NAME
-    FEATURE_LIBRARY_VERSION = "feature_library_v2"
+    FEATURE_LIBRARY_VERSION = "feature_library_v3"
 
     # Feature Selection Grid
-    # FEATURE_SELECTION_METHOD_NAMES = [
-    #     {
-    #         "all": {},
-    #         "kbest": {"k": [20]},
-    #         "variance": {"threshold": [0.1]},
-    #         "xgboost": {},
-    #     }
-    # ]
-    FEATURE_SELECTION_METHOD = ("xgboost", {})
+    FEATURE_SELECTION_METHOD = ("kbest", {"k": 20})
 
     # Target choice
     TARGET_NAME = "C.TARGET_SCORE_TOT"
