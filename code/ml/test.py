b4185513012e42c465e9f8d66fb091555187a4cb
Flore Vancompernolle Vromman <flore.vancompernolle@uclouvain.be>
2024-07-02 19:14:48 -0400

    Test on items of the target
    Computing factor analysis of the items of the target

---
+++ code/ml/test.py
@@ -0,0 +1,29 @@
+from loaders import load_attributes
+from constants import Constants as C
+from configs.create_feature import CreateFeatureConfig as Config
+
+from sklearn.decomposition import FactorAnalysis
+
+path = C.ML_PATH / "real"
+file_name = C.ATTRIBUTES_FILENAME
+df = load_attributes(path, file_name)
+def_reduced = df[[
+    'CSM_QA1_1',
+    'CSM_QA1_2',
+    'CSM_QA1_3',
+    'CSM_QA2_1',
+    'CSM_QA2_2',
+    'CSM_QA2_3',
+    'CSM_QA2_4',
+    'CSM_QA2_5',
+    'CSM_QA2_6',
+    'CSM_QA2_7',
+    'CSM_QA2_8',
+    'CSM_QA2_9',
+    'CSM_QA2_10',
+    'CSM_QA2_11',
+]]
+def_reduced = def_reduced.fillna(def_reduced.mean())
+transformer = FactorAnalysis(n_components=1)
+X_transformed = transformer.fit_transform(def_reduced)
+breakpoint()
