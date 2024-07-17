09497a41aa1185b424d40ab1d95ff237871865fb
LaurenceOMFoisy <mail@mfoisy.com>
2024-07-17 11:06:50 -0400

    update

--- code/cleaning/taille_gpt.R
+++ code/cleaning/taille_gpt.R
@@ -1,4 +1,11 @@
+library(dplyr)
+
 ## Taille ----------------------------------------------------------------------
+
+data_raw <- haven::read_sav("_SharedFolder_datagotchi-santé/data/raw/data_raw.sav") |> 
+  filter(code == "complete")
+
+data_taille <- data.frame(id = 1:nrow(data_raw))
 
 attributes(data_raw$taille)
 table(data_raw$taille)
@@ -7,24 +14,24 @@ table(data_raw$taille_1_TEXT)
 attributes(data_raw$taille_2_TEXT)
 table(data_raw$taille_2_TEXT)
 
-data_clean$ses_taille <- NA
+data_taille$ses_taille <- NA
 valid_indices_taille_1 <- which(data_raw$taille_1_TEXT != "")
 valid_indices_taille_2 <- which(data_raw$taille_2_TEXT != "")
-data_clean$ses_taille[valid_indices_taille_1] <- data_raw$taille_1_TEXT[valid_indices_taille_1]
-data_clean$ses_taille[valid_indices_taille_2] <- data_raw$taille_2_TEXT[valid_indices_taille_2]
-table(data_clean$ses_taille, useNA = "ifany")
+data_taille$ses_taille[valid_indices_taille_1] <- data_raw$taille_1_TEXT[valid_indices_taille_1]
+data_taille$ses_taille[valid_indices_taille_2] <- data_raw$taille_2_TEXT[valid_indices_taille_2]
+table(data_taille$ses_taille, useNA = "ifany")
 
-data_clean$ses_taille_gpt <- NA
+data_taille$ses_taille_gpt <- NA
 
-for (i in 1:nrow(data_clean)) {
+for (i in 1880:nrow(data_taille)) {
   
-    if (is.na(data_clean$ses_taille[i]) || data_clean$ses_taille[i] == "NA") {
+    if (is.na(data_taille$ses_taille[i]) || data_taille$ses_taille[i] == "NA") {
       next
     } else {
 
     system <- "your role is to output the height of respondents in CM based on given height information" # Donne un role au modèle. Doit être clair et concis
 
-    prompt <- paste0("Please read a survey answer representing the height of a survey respondent and convert it to centimeters. Output only a single number representing the height of the respondent in CM. The given value might be numeric or textual, might be in meters or in feet and inches. Your role is to convert the value to centimeters. Here is the respondant's height: ", data_clean$ses_taille[i]) # Ton prompt
+    prompt <- paste0("Please read a survey answer representing the height of a survey respondent and convert it to centimeters. Output only a single number representing the height of the respondent in CM. The given value might be numeric or textual, might be in meters, centimeters, or in feet and inches. Your role is to convert the value to centimeters. Use your own judgement to determine if the given value is in centimeters, meters or feet. It is the height of respondents so there shouldn't be extreme values. Here is the respondant's height: ", data_taille$ses_taille[i], ". If there are multiple scenarios, give me the result for the one you judge the most probable. It is VERY important that you output only a single number. NO TEXT.") # Ton prompt
   
     chat_prompt <- openai::create_chat_completion(
         model = "gpt-4-turbo",
@@ -38,11 +45,59 @@ for (i in 1:nrow(data_clean)) {
             )
     )
   
-  print(paste0("Taille: ", data_clean$ses_taille[i]))
-  print(paste0("Taille convertie: ", chat_prompt$choices$message.content))
-  Sys.sleep(1)
+  print(paste0("i = ",i ,". Taille: ", data_taille$ses_taille[i]))
+  print(paste0("i = ",i ,". Taille convertie: ", chat_prompt$choices$message.content))
 
-  data_clean$ses_taille_gpt[i] <- chat_prompt$choices$message.content
+  data_taille$ses_taille_gpt[i] <- chat_prompt$choices$message.content
 
   }
 }
+
+for (i in 1:nrow(data_taille)) {
+  
+    if (is.na(data_taille$ses_taille[i]) || data_taille$ses_taille[i] == "NA") {
+      print(paste0("i = ",i ,". Skip NA"))
+      next
+    }
+
+    if (as.numeric(data_taille$ses_taille_gpt[i]) < 250 & as.numeric(data_taille$ses_taille_gpt[i]) > 50) {
+      print(paste0("i = ",i ,". Skip cause OK"))
+      next
+    }
+
+    system <- "your role is to output the height of respondents in CM based on given height information" # Donne un role au modèle. Doit être clair et concis
+
+    prompt <- paste0("Please read a survey answer representing the height of a survey respondent and convert it to centimeters. Output only a single number representing the height of the respondent in CM. The given value might be numeric or textual, might be in meters, centimeters, or in feet and inches. Your role is to convert the value to centimeters. Use your own judgement to determine if the given value is in centimeters, meters or feet. It is the height of respondents so there shouldn't be extreme values. Here is the respondant's height: ", data_taille$ses_taille[i], ". If there are multiple scenarios, give me the result for the one you judge the most probable. It is VERY important that you output only a single number. NO TEXT.") # Ton prompt
+  
+    chat_prompt <- openai::create_chat_completion(
+        model = "gpt-4-turbo",
+        messages = list(
+            list("role" = "system",
+                 "content" = system
+            ),
+            list(
+                "role" = "user",
+                "content" = prompt)
+            )
+    )
+  
+  print(paste0("i = ",i ,". Taille: ", data_taille$ses_taille[i]))
+  print(paste0("i = ",i ,". Taille convertie: ", chat_prompt$choices$message.content))
+
+  data_taille$ses_taille_gpt[i] <- chat_prompt$choices$message.content
+
+}
+
+
+data_taille <- data_taille |> 
+  select(id, ses_taille_gpt) |> 
+  mutate(ses_taille = as.numeric(ses_taille_gpt)) |> 
+  select(-ses_taille_gpt)
+
+saveRDS(data_taille, "_SharedFolder_datagotchi-santé/data/clean/data_taille.rds")
+
+data_clean <- readRDS("_SharedFolder_datagotchi-santé/data/clean/datagotchi-sante_clean.rds")
+
+data_clean <- merge(data_clean, data_taille, by = "id")
+
+saveRDS(data_clean, "_SharedFolder_datagotchi-santé/data/clean/datagotchi-sante_clean.rds")
