83389240c4cdfc0e17ea67ab4584ff96c8bd2c89
Flore Vancompernolle Vromman <flore.vancompernolle@uclouvain.be>
2024-07-19 11:55:48 -0400

    Empty page

--- code/ml/pages/visuals_feature_saturation.py
+++ code/ml/pages/visuals_feature_saturation.py
@@ -5,3 +5,14 @@ import streamlit as st
 from configs.visuals import VisualsConfig as Config
 from constants import Constants as C
 from visuals import menu
+
+###################### Functions  ########################
+
+
+###################### Content page ##########################
+
+# Sidebarmenu
+menu()
+
+# Text
+st.title("Feature Selection Saturation")
