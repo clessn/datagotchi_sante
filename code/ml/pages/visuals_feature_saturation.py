04eed49b709dc05e77bc3c607cf775c1f14a9103
Flore Vancompernolle Vromman <flore.vancompernolle@uclouvain.be>
2024-07-19 12:12:48 -0400

    Select experiment

--- code/ml/pages/visuals_feature_saturation.py
+++ code/ml/pages/visuals_feature_saturation.py
@@ -8,6 +8,26 @@ from visuals import menu
 
 ###################### Functions  ########################
 
+### Select an experiment
+def select_experiment(experiments_path):
+
+    # Title of sidebar
+    st.sidebar.title("Selection of the experiment")
+
+    # Select the experiment
+    experiments_list = [experiment for experiment in os.listdir(experiments_path)]
+    experiments_list_sorted = sorted(experiments_list, reverse=True)
+    selected_experiment = st.sidebar.selectbox(
+        "Choose the experiment you want to see", experiments_list_sorted
+    )
+    return selected_experiment
+
+
+###################### Preloading  ########################
+
+# Derive paths from configs
+ml_run_path = C.ML_PATH / eval(f"C.{Config.RUN_TYPE}")
+experiments_path = ml_run_path / C.EXPERIMENTS_FOLDER_NAME
 
 ###################### Content page ##########################
 
@@ -16,3 +36,6 @@ menu()
 
 # Text
 st.title("Feature Selection Saturation")
+
+# Experiment selection
+select_experiment(experiments_path)
