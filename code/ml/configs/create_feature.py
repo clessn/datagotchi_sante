54e0aec59b1fa3ad408f3d027100ea190d77b178
Corentin Vande Kerckhove <corentinvdk@gmail.com>
2024-07-25 14:17:24 -0400

    commit single encoding and regressor alpha increase

--- code/ml/configs/create_feature.py
+++ code/ml/configs/create_feature.py
@@ -2,3 +2,9 @@ class CreateFeatureConfig:
     # Versioning
     RUN_TYPE = "REAL_FOLDER_NAME"  # SANDBOX_FOLDER_NAME or REAL_FOLDER_NAME
     CODEBOOK_VERSION = "frozen_codebook_june_21.csv"
+
+    # Target encoding
+    TARGET_ENCODING_SINGLE_NOMINAL = True
+    TARGET_ENCODING_MULTIPLE_NOMINAL = True
+    TARGET_NAME = "C.TARGET_SCORE_TOT"
+
