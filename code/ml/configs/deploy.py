91f6f9f7e6dc968770f7b7bdfb150f05ca770386
Corentin Vande Kerckhove <corentinvdk@gmail.com>
2024-07-25 14:32:15 -0400

    create a deploy config

---
+++ code/ml/configs/deploy.py
@@ -0,0 +1,5 @@
+class DeployConfig:
+    RUN_TYPE = "REAL_FOLDER_NAME"  # SANDBOX_FOLDER_NAME or REAL_FOLDER_NAME
+    CODEBOOK_VERSION = "frozen_codebook_july_25.csv"
+    FEATURE_SELECTION_FILE = "feature_library_v4/feature_selection_xgboost_k_20.csv"
+
