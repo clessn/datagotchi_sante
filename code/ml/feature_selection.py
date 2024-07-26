01087d5a56bec77a71f0d8bc4e02579ff5221ecc
Flore Vancompernolle Vromman <flore.vancompernolle@uclouvain.be>
2024-07-26 18:15:27 -0400

    Count features

--- code/ml/feature_selection.py
+++ code/ml/feature_selection.py
@@ -79,38 +79,12 @@ def select_k_best_features(df_X, df_y, k):
     feature_scores = score_normalization(feature_scores)
     
     # Create a dataframe with features selected
-    df_features = feature_selected(feature_scores, df_X)
+    df_features = feature_selected(feature_scores, df_X, k)
     
     #threshold = np.partition(feature_scores, -k)[-k]
     #feature_selected = (feature_scores >= threshold).astype(int)
 
     return df_features['feature_scores'], df_features['feature_selected']
-
-
-# Create a dataframe with features selected
-def feature_selected(feature_scores, df_X):
-
-    # Create dataframe
-    dico_features = {'feature_names': df_X.columns.tolist(), 'feature_scores': feature_scores}
-    df_features = pd.DataFrame(dico_features)
-
-    # Order by scores
-    df_features_sorted = df_features.sort_values(by='feature_scores')
-
-    # Derive paths from configs
-    ml_run_path = C.ML_PATH / eval(f"C.{Config.RUN_TYPE}")
-    frozen_library_folder_name = Config.FEATURE_LIBRARY_VERSION
-    feature_library_path = (
-        ml_run_path / C.FEATURE_LIBRARIES_FOLDER_NAME / frozen_library_folder_name
-    )
-
-    # Feature lookup table
-    df_feature_lookup = load_feature_lookup_table(feature_library_path, C.FEATURE_LOOKUP_FILENAME)
-
-    # Merge the two dataframes
-    df_feature_score_lookup = df_features_sorted.merge(df_feature_lookup, on='feature_names', how='inner')
-
-    return df_feature_score_lookup
 
 
 # Feature selection based on xgboost
@@ -133,10 +107,42 @@ def select_xgboost_features(df_X, df_y, k):
     # Normalize scores
     feature_scores = score_normalization(feature_importances)
 
-    # Selected features
-    feature_selected = selector.get_support()
+    # Create a dataframe with features selected
+    df_features = feature_selected(feature_scores, df_X, k)
 
-    return feature_scores, feature_selected
+    return df_features['feature_scores'], df_features['feature_selected']
+
+
+# Create a dataframe with features selected
+def feature_selected(feature_scores, df_X, k):
+
+    # Create dataframe
+    dico_features = {'feature_names': df_X.columns.tolist(), 'feature_scores': feature_scores}
+    df_features = pd.DataFrame(dico_features)
+
+    # Order by scores
+    df_features_sorted = df_features.sort_values(by='feature_scores')
+
+    # Derive paths from configs
+    ml_run_path = C.ML_PATH / eval(f"C.{Config.RUN_TYPE}")
+    frozen_library_folder_name = Config.FEATURE_LIBRARY_VERSION
+    feature_library_path = (
+        ml_run_path / C.FEATURE_LIBRARIES_FOLDER_NAME / frozen_library_folder_name
+    )
+
+    # Feature lookup table
+    df_feature_lookup = load_feature_lookup_table(feature_library_path, C.FEATURE_LOOKUP_FILENAME)
+
+    # Merge the two dataframes
+    df_feature_score_lookup = df_features_sorted.merge(df_feature_lookup, on='feature_names', how='inner')
+
+    # Count unique id
+    df_feature_score_lookup['count_unique_id_feature'] = df_feature_score_lookup['id'].expanding().apply(lambda x: x.nunique())
+
+    # Select features
+    df_feature_score_lookup['feature_selected'] = (df_feature_score_lookup['count_unique_id_feature'] <= k).astype(int)
+
+    return df_feature_score_lookup
 
 
 available_feature_selection = {
