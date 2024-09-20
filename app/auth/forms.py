054b59b1f7dc6feadd6715362054a92446f233f9
Flore Vancompernolle Vromman <flore.vancompernolle@uclouvain.be>
2024-09-20 10:13:45 +0200

    Removing old code to close app

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -6,6 +6,3 @@ class LoginForm(FlaskForm):
     code = StringField('Code', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-# Button to quit and log out
-class Close(FlaskForm):
-    submit = SubmitField('Finish and go to survey') 
