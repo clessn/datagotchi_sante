8f47ac53666c59964e2d5afb474101ec46069e3a
Flore Vancompernolle Vromman <flore.vancompernolle@uclouvain.be>
2025-04-07 18:07:41 +0200

    compute quartiles

--- app/ml/eda.py
+++ app/ml/eda.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from app.ml.constants import Constants as C
 from app.ml.configs.eda import EdaConfig as Config
 from app.ml.loaders import load_targets
@@ -9,4 +11,12 @@ def score_eda():
         ml_run_path / C.FEATURE_LIBRARIES_FOLDER_NAME / frozen_library_folder_name, 
         C.TARGETS_FILENAME,
     )
-    print(df_y)
+    # Rescale to have values 0-100 instead of 0-70
+    df_y['score_tot_scaled'] = df_y['score_tot'] * (100 / 70)
+
+    # Compute quartiles 
+    quartiles = df_y['score_tot_scaled'].quantile([0.25, 0.5, 0.75]).round().astype(int)
+    X1 = quartiles[0.25]
+    X2 = quartiles[0.5]  
+    X3 = quartiles[0.75]
+    print(f"The quartiles are: X1 = {X1:.2f}, X2 = {X2:.2f}, X3 = {X3:.2f}.")
