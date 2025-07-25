5046705cadeb738ae46616f18461d59f25a46b5b
Flore Vancompernolle Vromman <flore.vancompernolle@uclouvain.be>
2025-07-25 14:31:39 +0200

    study_id in config

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ class DefaultConfig(Config):
     MAIN_PAGE='main.consent'
     SKIP_VALID=False
     EXPLAIN_TYPE=None
-    #PROLIFIC_STUDY_ID='687a244efed49cdf9e6592ff' # Final study ID
-    #PROLIFIC_COMPLETION_CODE='CRQYIPFI'# Final completion code
+    PROLIFIC_STUDY_ID='687a244efed49cdf9e6592ff' # Final study ID
+    PROLIFIC_COMPLETION_CODE='CRQYIPFI'# Final completion code
 
 class DebugConfig(Config):
     MAIN_PAGE='main.consent'
@@ -28,8 +28,8 @@ class DebugConfig(Config):
     #EXPLAIN_TYPE='explain_interactive'
     # choose amongts 'explain_baseline', 'explain_visual',
     # 'explain_textual', 'explain_quantitative', 'explain_interactive', 'explain_contextual'
-    #PROLIFIC_STUDY_ID='687a244efed49cdf9e6592ff' # Final study ID
-    #PROLIFIC_COMPLETION_CODE='CRQYIPFI'# Final completion code
+    PROLIFIC_STUDY_ID='687a244efed49cdf9e6592ff' # Final study ID
+    PROLIFIC_COMPLETION_CODE='CRQYIPFI'# Final completion code
 
 configs = {
   'default'  : DefaultConfig,
