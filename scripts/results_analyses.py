a1251f26cffcac8aa470294182a639caea9d18ba
Flore Vancompernolle Vromman <flore.vancompernolle@uclouvain.be>
2025-08-12 19:22:11 +0200

    save results

--- scripts/results_analyses.py
+++ scripts/results_analyses.py
@@ -353,3 +353,6 @@ def clean_results():
 
     print(results_df.columns.tolist())
     print(results_df.head())
+    # save results_df to csv file
+    clean_path = Path(os.getenv("DATA_RESULT_PATH")) / CLEAN_FOLDER
+    results_df.to_csv(clean_path / 'clean_results.csv', index=False)
