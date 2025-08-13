9f98131139dc5b4936f9d3e09d0ff55fa5cb4215
Flore Vancompernolle Vromman <flore.vancompernolle@uclouvain.be>
2025-08-13 11:47:08 +0200

    load data

--- explain_study.R
+++ explain_study.R
@@ -1,13 +1,57 @@
-library(readr)
+###############
+# Packages 
+###############
 
-# Reand path from environment variable
+# packages installation for the first time only
+install.packages("readr")
+install.packages("dotenv")
+install.packages("lubridate")
+
+# packages loading
+library(readr) # reading csv files
+library(dotenv) # loading environment variables from .env file
+library(lubridate) # for date manipulation
+
+###############
+# Loading data 
+###############
+
+# read path from environment variable
 data_result_path <- Sys.getenv("DATA_RESULT_PATH")
 
-# Build the path to the file with results
+# build the path to the file with results
 clean_results_file_path <- file.path(data_result_path, "clean", "clean_results.csv")
 
-# Read file
-clean_results <- read_csv(file_path)
+# read file
+clean_results <- read_csv(clean_results_file_path)
 
-# Show the head of the data
+# convert columns to appropriate types
+# duration, remove date part and convert to duration
+clean_results$study_duration <- hms(sub(".* ", "", clean_results$study_duration))
+# factor
+cols_to_factor <- c(
+  "explain_type",
+  "sociodemo_01",
+  "sociodemo_02",
+  "sociodemo_03",
+  "sociodemo_05",
+  "sociodemo_07",
+  "sociodemo_08"
+)
+clean_results[cols_to_factor] <- lapply(clean_results[cols_to_factor], as.factor)
+
+# show the head of the data
 head(clean_results)
+sapply(clean_results, class)
+
+###############
+# Manipulation checks
+###############
+
+# To complete
+
+###############
+# Main models
+###############
+
+# To complete
